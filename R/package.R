9c4c4df9f0e9e70f90f2205a76983351eaa386ef
tichelbaecker <thomastichelbaecker@gmail.com>
2024-05-06 15:00:31 +0200

    v0.1 working locally

---
+++ R/package.R
@@ -0,0 +1,16 @@
+#' @keywords internal 
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+#' @import shiny
+#' @import dplyr
+#' @import stringr
+#' @import shinyWidgets
+#' @import pdftools
+#' @import magick
+#' @import tesseract
+#' @import googleCloudVisionR
+## usethis namespace: end
+NULL
