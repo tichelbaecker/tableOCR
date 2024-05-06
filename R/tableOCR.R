2cab46faef5c1ca1383ddf3a8970afaad2d2740b
tichelbaecker <thomastichelbaecker@gmail.com>
2024-05-06 14:30:08 +0200

    v0.1 edit

--- R/tableOCR.R
+++ R/tableOCR.R
@@ -5,14 +5,6 @@
 
 ###### Written by Thomas Tichelbaecker, Princeton University
 
-#' Fahrenheit conversion
-#'
-#' Convert degrees Fahrenheit temperatures to degrees Celsius
-#' @param F_temp The temperature in degrees Fahrenheit
-#' @return The temperature in degrees Celsius
-#' @examples 
-#' temp1 <- F_to_C(50);
-#' temp2 <- F_to_C( c(50, 63, 23) );
 #' @export
 
 table_ocr <- function(auth_file_path){
