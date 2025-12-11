eeef74b167c3b5a25d1e0eca5ab4b16985fbc459
tichelbaecker <thomastichelbaecker@gmail.com>
2025-12-11 21:43:30 +0100

    fix pipes

--- R/tableOCR.R
+++ R/tableOCR.R
@@ -347,8 +347,8 @@ server <- function(input, output) {
           path = path_page,
           density = 200
         ) |> 
-          #image_rotate(., input$rotate) |> 
-          image_convert(., colorspace = "Gray") 
+          image_rotate(input$rotate) |> 
+          image_convert(colorspace = "Gray") 
         
         # 1) Render the PDF page to a bitmap using pdftools
         # bitmap <- pdftools::pdf_render_page(
@@ -375,8 +375,8 @@ server <- function(input, output) {
         
         # load image
         img <- image_read(f$datapath) |> 
-          #image_rotate(., input$rotate) |> 
-          image_convert(., colorspace = "Gray") 
+          image_rotate(input$rotate) |> 
+          image_convert(colorspace = "Gray") 
           # image_threshold(type = "black", threshold = "65%") %>%
           # image_threshold(type = "white", threshold = "65%") %>%
           #image_deskew() %>%
